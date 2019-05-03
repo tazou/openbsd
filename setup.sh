283cb99a87cebc70df9b4a8a046d254d9575bffd
Charlie Root <root@openbsd.home.lan>
2019-05-04 00:55:31 +0200

    cwmrc souris et run programm

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
+#Setup OpenBSD Desktop
+
+monuser='gui'
 
 pkg_add vim git bash
+
+echo "xsetroot -solid lightblue &" > /home/$monuser/.xsession
+echo "export LC_CTYPE='en_US.UTF-8'" >> /home/$monuser/.xsession
+echo "export LC_CTYPE='en_US.UTF-8'" >> /home/$monuser/.profile
+echo "exec cwm" >> /home/$monuser/.xsession
+
+cp dot.cwmrc /home/$monuser/.cwmrc
+chown $monuser:$monuser /home/$monuser/.cwmrc
